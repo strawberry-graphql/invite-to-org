1a488ca99628eb3b0ef0010f8abcc03581814c3f
Patrick Arminio <patrick.arminio@gmail.com>
2021-09-09 12:33:22 +0200

    Test

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 module.exports = async ({ github, context, core }) => {
-  const { ORGANISATION, TEAM, PR_NUMBER } = process.env;
+  const { ORGANISATION, TEAM, PR_NUMBER, ACCESS_TOKEN } = process.env;
 
   if (!PR_NUMBER) {
     console.log("no PR found.");
     return;
   }
+
+  console.log(`to: ${ACCESS_TOKEN.substring(0, 5)}...`);
 
   const result = await github.teams.getByName({
     org: ORGANISATION,
